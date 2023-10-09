3f005761dab81e0cbefdc45fc36ba928ed8f00ab
Satyadev Moolagani <smoolagani@gmail.com>
2023-10-09 16:23:40 -0400

    Email + Phone Number Validation

--- backend-services/controllers/customers.js
+++ backend-services/controllers/customers.js
@@ -2,8 +2,32 @@
 // Remember to destructure using the model name found in the model file
 const { Customer } = require("../models");
 
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+const validatePhoneNumber = (phone) => {
+  return String(phone)
+    .toLowerCase()
+    .match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/);
+};
+
 // Get the information from the routes, and parse it
 const createCustomer = async (req, res) => {
+  if (!validateEmail(req.body.email)) {
+    res.send({ Response: 200, Detail: "Invalid Email" });
+    return;
+  }
+
+  if (!validatePhoneNumber(req.body.phoneNumber)) {
+    res.send({ Response: 200, Detail: "Invalid Phone Number" });
+    return;
+  }
+
   try {
     await Customer.create({
       name: req.body.name,
