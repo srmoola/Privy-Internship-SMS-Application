365b184c293dc2145579c5dae36727798c3573c7
Satyadev Moolagani <smoolagani@gmail.com>
2023-10-16 21:55:15 -0400

    Dashboard + Get Data

--- backend-services/routes/checkUser.js
+++ backend-services/routes/checkUser.js
@@ -2,7 +2,9 @@ var express = require("express"),
   router = express.Router();
 
 const checkUser = require("../controllers/checkUser");
+const getUserInfo = require("../controllers/getUserInfo");
 
 router.post("/", checkUser);
+router.post("/get-user-info", getUserInfo);
 
 module.exports = router;
--- frontend-application/src/routes/mainpage.tsx
+++ frontend-application/src/routes/mainpage.tsx
@@ -1,14 +1,41 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+import axios from "axios";
 
 const MainPage = () => {
-  const [userID, setuserID] = useState<string>("");
-
+  const [userInfo, setuserInfo] = useState<any>(null);
+  const navigate = useNavigate();
   useEffect(() => {
     const user_id = localStorage.getItem("USER_ID");
-    setuserID(user_id!);
+    if (!user_id) {
+      navigate("/error");
+      return; // Early return to prevent further execution if there's no USER_ID
+    }
+
+    const fetchData = async () => {
+      try {
+        const response = await axios({
+          method: "post",
+          url: "http://localhost:3000/check-user/get-user-info",
+          data: {
+            USER_ID: user_id,
+          },
+        });
+        if (response.data) {
+          setuserInfo(response.data);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  return <div>{userID}</div>;
+  console.log(userInfo);
+
+  return <Dashboard username={!userInfo ? "User" : userInfo.firstName} />;
 };
 
 export default MainPage;
